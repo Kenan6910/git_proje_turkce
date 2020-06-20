f18b83bb44c06ab40ffc0d13f68c2bb43324f5a1
Kenan6910 <67188285+Kenan6910@users.noreply.github.com>
2020-06-20 22:30:04 +0300

    Update FirstClass.java
    conflit yaptik galiba

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
         System.out.println("first commit from local repositorty");
         System.out.println("asfdahdgfkjsfkla");
-        System.out.println("jasccKLDHChdccahC");
+        System.out.println("conflict yapmak istiyoruz");
 	System.out.println("first commit from remote repositorty");
 	}
 
