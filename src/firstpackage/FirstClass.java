f336086e7067e0dd64f3f6ef8c833636d16a7f75
Kenan6910 <67188285+Kenan6910@users.noreply.github.com>
2020-06-20 22:15:05 +0300

    Update FirstClass.java

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,9 +3,10 @@ package firstpackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("first commit from local repositorty");
+        System.out.println("first commit from local repositorty");
         System.out.println("asfdahdgfkjsfkla");
         System.out.println("jasccKLDHChdccahC");
+	System.out.println("first commit from remote repositorty");
 	}
 
 }
