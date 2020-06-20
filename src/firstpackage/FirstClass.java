0bf74a91c91e9a1b591c46eae4f3be8b5af7d6fc
Kenan <kkulak7910@gmail.com>
2020-06-20 22:27:57 +0300

    conflit olusturma

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,8 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
         System.out.println("first commit from local repositorty");
         System.out.println("asfdahdgfkjsfkla");
-        System.out.println("jasccKLDHChdccahC");
-	System.out.println("first commit from remote repositorty");
+        System.out.println("ucuncu commitimiz local repositorty");
+	    System.out.println("first commit from remote repositorty");
 	}
 
 }
